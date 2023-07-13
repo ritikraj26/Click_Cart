7e56e1f8c823e4a105c3fd17f901e3de843b15a6
Ritik Raj <ritikraj2699@outlook.com>
2023-07-14 01:29:21 +0530

    created contetx processors

--- store/views.py
+++ store/views.py
@@ -3,12 +3,6 @@ from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
 
 
-def categories(request):
-    return {
-        'categories':Category.objects.all()
-    }
-
-
 def all_products(request):
     products = Product.objects.all()
     return render(request,'store/home.html',{'products':products})
