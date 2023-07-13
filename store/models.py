cc01e8c108df282219a6d80a6dea37180947171f
Ritik Raj <ritikraj2699@outlook.com>
2023-07-14 03:24:14 +0530

    added static path

--- store/models.py
+++ store/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.db.models.query import QuerySet
 from django.urls import reverse
 
+
 class ProductManager(models.Manager):
     def get_queryset(self):
         return super(ProductManager,self).get_queryset().filter(is_active=True)
